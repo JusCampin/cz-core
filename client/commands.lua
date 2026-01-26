5a118567c82b3cc7901827aa2407325e8c22c6e7
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-01-25 20:41:10 -0800

    Update commands.lua

--- client/commands.lua
+++ client/commands.lua
@@ -1,38 +1,5 @@
 RegisterCommand('menu', function()
-    OpenMainMenu()
-end, false)
-
-RegisterCommand('carSpawn', function(source, args, rawCommand)
-    local model = args[1] or 'adder'
-
-    if not IsModelInCdimage(model) or not IsModelAVehicle(model) then
-        TriggerEvent('chat:addMessage', {
-            args = { 'Error: ' .. model .. ' is not a valid vehicle' }
-        })
-        return
-    end
-
-    RequestModel(model, false)
-    while not HasModelLoaded(model) do
-        Wait(10)
-    end
-
-    local playerPed = PlayerPedId()
-    local playerCoords = GetEntityCoords(playerPed)
-    local heading = GetEntityHeading(playerPed)
-
-    local myCar = CreateVehicle(model, playerCoords.x, playerCoords.y, playerCoords.z, heading, true, false)
-    SetPedIntoVehicle(playerPed, myCar, -1)
-    SetModelAsNoLongerNeeded(model)
-end, false)
-
-RegisterCommand('carDelete', function()
-    local playerPed = PlayerPedId()
-    local vehicle = GetVehiclePedIsIn(playerPed, false)
-
-    if DoesEntityExist(vehicle) then
-        DeleteEntity(vehicle)
-    end
+    MainMenu()
 end, false)
 
 RegisterCommand('+openhood', function()
@@ -75,17 +42,3 @@ end, false)
 
 RegisterKeyMapping('+openhood', 'Open Vehicle Hood', 'keyboard', 'PAGEUP')
 RegisterKeyMapping('+opentrunk', 'Open Vehicle Trunk', 'keyboard', 'PAGEDOWN')
-
-RegisterCommand('gun', function(source, args)
-    local gunName = args[1] or 'WEAPON_APPISTOL'
-
-    if not IsWeaponValid(joaat(gunName)) then
-        TriggerEvent('chat:addMessage', {
-            args = { 'Error: ' .. gunName .. ' is not a valid model' }
-        })
-        return
-    end
-
-    local player = PlayerPedId()
-    GiveWeaponToPed(player, joaat(gunName), 200, false, true)
-end, false)
