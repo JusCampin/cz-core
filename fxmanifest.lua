280170d929166c9d7ecf8c31a1711d21a2e60d73
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-01-25 20:41:37 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'CoreZ Team'
 description 'Core resource for CoreZ Framework'
-version '1.0.0'
+version '0.1.0'
 
 shared_scripts {
     'configs/config.lua',
@@ -13,12 +13,17 @@ client_scripts {
     'client/rpc.lua',
     'client/main.lua',
     'client/commands.lua',
-    'client/menu.lua',
+    'client/menus/menu_init.lua',
+    'client/menus/spawn_menu.lua',
+    'client/menus/car_menu.lua',
+    'client/menus/gun_menu.lua',
+    'client/menus/main_menu.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/rpc.lua',
+    'server/setup.lua',
     'server/main.lua',
 }
 
