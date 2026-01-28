75cf14db70f05dfcd02635789ff2f2e2b5977e2d
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-01-27 18:30:51 -0800

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,5 @@
+----------------------------------
+-- EXAMPLE CLIENT RPC USAGE
 if not CZ_RPC_CLIENT then
 	print('[cz-core] WARNING: CZ_RPC_CLIENT not found')
 else
@@ -13,23 +15,4 @@ else
 		end)
 	end)
 end
-
-local spawnPos = vector3(-540.58, -212.02, 37.65)
-
-AddEventHandler('onClientGameTypeStart', function()
-    exports.spawnmanager:setAutoSpawnCallback(function()
-        exports.spawnmanager:spawnPlayer({
-            x = spawnPos.x,
-            y = spawnPos.y,
-            z = spawnPos.z,
-            model = 'a_m_m_skater_01'
-        }, function()
-            TriggerEvent('chat:addMessage', {
-                args = { 'Welcome to Apos Test Server!' }
-            })
-        end)
-    end)
-
-    exports.spawnmanager:setAutoSpawn(true)
-    exports.spawnmanager:forceRespawn()
-end)
+----------------------------------
