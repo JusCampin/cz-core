6bb932fe968d891e99baaf553c25f17b1ee71cb3
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-01-27 18:30:54 -0800

    Update car_menu.lua

--- client/menus/car_menu.lua
+++ client/menus/car_menu.lua
@@ -2,7 +2,7 @@ function CarMenu()
     local CarPage = CoreMenu:RegisterPage('car:page')
 
     CarPage:RegisterElement('header', {
-        value = 'Car Menu',
+        value = 'Vehicle Menu',
         slot = "header",
         style = {
             ['color'] = '#999'
@@ -10,7 +10,7 @@ function CarMenu()
     })
 
     CarPage:RegisterElement('subheader', {
-        value = "Select an option below",
+        value = "Select an Option Below",
         slot = "header",
         style = {
             ['font-size'] = '0.94vw',
@@ -23,14 +23,35 @@ function CarMenu()
         style = {}
     })
 
+    local model
+    CarPage:RegisterElement('dropdown', {
+        label = 'Select Model',
+        slot = "content",
+        style = {
+                ['color'] = '#E0E0E0'
+            },
+        options = {
+            { text = "Hustler", value = "hustler" },
+            { text = "Comet Retro", value = "comet3"},
+            { text = "Trophy Truck", value = "trophytruck"},
+            { text = "Zombie Bobber", value = "zombiea"},
+            { text = "Sanchez", value = "sanchez2"},
+            { text = "Seashark", value = "seashark"},
+            { text = "Frogger", value = "frogger"},
+            { text = "Dodo", value = "dodo"},
+        },
+    }, function(data)
+        model = data.value
+    end)
+
     CarPage:RegisterElement('button', {
-        label = "Spawn Car",
+        label = "Spawn",
         slot = 'content',
         style = {
             ['color'] = '#E0E0E0'
         },
     }, function()
-        local model = 'adder'
+        --local model = 'adder'
 
         if not IsModelInCdimage(model) or not IsModelAVehicle(model) then
             print('Invalid vehicle model: ' .. model)
@@ -53,7 +74,7 @@ function CarMenu()
     end)
 
     CarPage:RegisterElement('button', {
-        label = "Delete Car",
+        label = "Delete",
         slot = 'content',
         style = {
             ['color'] = '#E0E0E0'
